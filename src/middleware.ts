b236c3e1228e2cb51c90735d0ce622f3eddf8162
saquibsid <saquib155@gmail.com>
2025-02-11 12:18:14 +0530

    revert middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
     const access_token_csai_id = req.cookies.get("access_token_csai_id")?.value;
-    if (!access_token_csai_id) {
-        return NextResponse.redirect('https://createstudio.app/login');
-    }
+    console.log("access_token_csai_id", access_token_csai_id);
+    // if (!access_token_csai_id) {
+    //     return NextResponse.redirect('https://createstudio.app/login');
+    // }
 }
 
 // Protect ALL routes except static assets
